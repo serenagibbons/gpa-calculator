d6f2792354d208f85428205d4e6d5711aa537467
Serena Gibbons <serenalgibbons@gmail.com>
2019-09-13 23:15:42 -0400

    Implement computeGPA method

--- app/src/main/java/com/example/gpacalculator/MainActivity.java
+++ app/src/main/java/com/example/gpacalculator/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.gpacalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText g1, g2, g3, g4, g5;
+    TextView display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        g1 = findViewById(R.id.grade1);
+        g2 = findViewById(R.id.grade2);
+        g3 = findViewById(R.id.grade3);
+        g4 = findViewById(R.id.grade4);
+        g5 = findViewById(R.id.grade5);
+        display = findViewById(R.id.gpa);
+    }
+
+    public void computeGPA(View view) {
+        double x1 = Double.parseDouble(g1.getText().toString());
+        double x2 = Double.parseDouble(g2.getText().toString());
+        double x3 = Double.parseDouble(g3.getText().toString());
+        double x4 = Double.parseDouble(g4.getText().toString());
+        double x5 = Double.parseDouble(g5.getText().toString());
+
+        double gpa = (x1+x2+x3+x4+x5)/5;
+        String strGpa = String.format(getResources().getString(R.string.gpa_display), gpa);
+        display.setText(strGpa);
     }
 }
